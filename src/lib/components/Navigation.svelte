f336a2e2de31e30c748f08b53fe6398dc180b3e7
dmehra2102 <deepanshumehra2102@gmail.com>
2024-01-28 00:16:56 +0530

    feat: improved menu accessibility throguh keyboard

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import type { ComponentType } from 'svelte';
+	import { tick, type ComponentType } from 'svelte';
 	import logo from '$assets/Spotify_Logo_RGB_White.png';
 	import { Home, Search, ListMusic, type Icon } from 'lucide-svelte';
+	import { fade } from 'svelte/transition';
+	import { beforeNavigate } from '$app/navigation';
 
 	export let desktop: boolean;
+
+	let isMobileMenuOpen: boolean = false;
+	let openMenuButton: HTMLButtonElement;
+	let closeMenuButton: HTMLButtonElement;
+	let lastFocusableElement: HTMLAnchorElement;
+	$: isOpen = desktop || isMobileMenuOpen;
 
 	const menuItems: { path: string; label: string; icon: ComponentType<Icon> }[] = [
 		{
@@ -23,26 +31,107 @@
 			icon: ListMusic
 		}
 	];
+
+	const openMenu = async () => {
+		isMobileMenuOpen = true;
+		await tick();
+		closeMenuButton.focus();
+	};
+
+	const closeMenu = async () => {
+		isMobileMenuOpen = false;
+		await tick();
+		openMenuButton.focus();
+	};
+
+	// We are creating this functions for making Menu accessable through keyboard
+	const moveFocusToBottom = (e: KeyboardEvent) => {
+		if (desktop) return;
+		if (e.key === 'Tab' && e.shiftKey) {
+			e.preventDefault();
+			lastFocusableElement.focus();
+		}
+	};
+	const moveFocusToTop = (e: KeyboardEvent) => {
+		if (desktop) return;
+		if (e.key === 'Tab' && !e.shiftKey) {
+			e.preventDefault();
+			closeMenuButton.focus();
+		}
+	};
+
+	const handleEscape = (e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			closeMenu();
+		}
+	};
+
+	// We are using this beforeNaigate method to close our sidebar first, before moving to some other pages.
+	beforeNavigate(() => {
+		isMobileMenuOpen = false;
+	});
 </script>
 
+<svelte:head>
+	<!-- We are doing this to stop background scrolling when sidebar is open .-->
+	{#if !desktop && isMobileMenuOpen}
+		<style>
+			body {
+				overflow: hidden;
+			}
+		</style>
+	{/if}
+</svelte:head>
+
 <div class="nav-content" class:desktop class:mobile={!desktop}>
+	{#if !desktop && isMobileMenuOpen}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div
+			class="overlay"
+			on:click={closeMenu}
+			transition:fade={{ duration: 200 }}
+			on:keyup={handleEscape}
+		/>
+	{/if}
 	<nav aria-label="Main">
-		<div class="nav-content-inner">
+		{#if !desktop}
+			<button bind:this={openMenuButton} aria-expanded={isOpen} on:click={openMenu}>Open</button>
+		{/if}
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div
+			class="nav-content-inner"
+			class:is-hidden={!isOpen}
+			style:visibility={isOpen ? 'visible' : 'hidden'}
+			on:keyup={handleEscape}
+		>
+			{#if !desktop}
+				<button bind:this={closeMenuButton} on:click={closeMenu} on:keydown={moveFocusToBottom}
+					>Close</button
+				>
+			{/if}
 			<img src={logo} alt="logo" class="logo" />
 			<ul>
-				{#each menuItems as item}
+				{#each menuItems as item, index}
+					{@const iconProps = {
+						focusable: 'false',
+						'aria-hidden': true,
+						color: 'var(--text-color)',
+						size: 26,
+						strokeWidth: 2
+					}}
 					<li class:active={item.path === $page.url.pathname}>
-						<a href={item.path}>
-							<svelte:component
-								this={item.icon}
-								focusable="false"
-								aria-hidden="true"
-								color="var(--text-color)"
-								size={26}
-								strokeWidth={2}
-							/>
-							{item.label}
-						</a>
+						{#if menuItems.length === index + 1}
+							<a href={item.path} bind:this={lastFocusableElement} on:keydown={moveFocusToTop}>
+								<svelte:component this={item.icon} {...iconProps} />
+								{item.label}
+							</a>
+						{:else}
+							<a href={item.path}>
+								<svelte:component this={item.icon} {...iconProps} />
+								{item.label}
+							</a>
+						{/if}
 					</li>
 				{/each}
 			</ul>
@@ -52,6 +141,19 @@
 
 <style lang="scss">
 	.nav-content {
+		.overlay {
+			position: fixed;
+			width: 100%;
+			height: 100%;
+			top: 0;
+			left: 0;
+			background-color: var(--sidebar-color);
+			opacity: 0.7;
+			z-index: 100;
+			@include breakpoint.up('md') {
+				display: none;
+			}
+		}
 		.logo {
 			max-width: 100%;
 			width: 130px;
@@ -102,6 +204,26 @@
 				@include breakpoint.up('md') {
 					display: block;
 				}
+			}
+		}
+		&.mobile .nav-content-inner {
+			position: fixed;
+			top: 0;
+			left: 0;
+			z-index: 100;
+			transition:
+				transform 200ms,
+				opacity 200ms;
+			&.is-hidden {
+				transition:
+					transform 200ms,
+					opacity 200ms,
+					visibility 200ms;
+				transform: translateX(-100%);
+				opacity: 0;
+			}
+			@include breakpoint.down('md') {
+				display: block;
 			}
 		}
 	}
