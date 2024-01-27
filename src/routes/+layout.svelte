96c1a0589af14f3b423aa0d9487f67d6f3b93d83
dmehra2102 <deepanshumehra2102@gmail.com>
2024-01-27 12:18:48 +0530

    feat: desktop sidebar ui done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,18 @@
 	import '../styles/main.scss';
 	import 'modern-normalize/modern-normalize.css';
 	import type { LayoutData } from './$types';
+	import { Navigation } from '$components';
 
 	export let data: LayoutData;
 	$: user = data?.user;
 </script>
 
 <div id="main">
+	{#if user}
+		<div id="sidebar">
+			<Navigation desktop={true} />
+		</div>
+	{/if}
 	<div id="content">
 		<main id="main-content">
 			<slot />
@@ -17,7 +23,9 @@
 
 <style lang="scss">
 	#main {
+		display: flex;
 		#content {
+			flex: 1;
 			main#main-content {
 				padding: 30px 15px 60px;
 				@include breakpoint.up('md') {
