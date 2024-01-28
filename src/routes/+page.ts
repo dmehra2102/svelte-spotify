6fa0817b8087e43c98b1e5f345a79c9a18f4c726
dmehra2102 <deepanshumehra2102@gmail.com>
2024-01-28 19:34:22 +0530

    feat: refershing access token during client side navigation

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,6 +1,8 @@
+import { fetchRefresh } from '$helpers';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch, parent }) => {
+export const load: PageLoad = async ({ fetch: _fetch, parent }) => {
+	const fetch = (path: string) => fetchRefresh(_fetch, path);
 	const { user } = await parent();
 	const new_releases = fetch('/api/spotify/browse/new-releases?limit=6');
 	const featured_playlists = fetch('/api/spotify/browse/featured-playlists?limit=6');
